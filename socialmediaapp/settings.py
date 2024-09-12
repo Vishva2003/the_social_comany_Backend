48f732f8dcb430da3f2dfb1487773b8a6d8cc8da
Vishva15Vishva2003 <vishvavishva587@gmail.com>
2024-09-12 12:43:16 +0530

    host

--- socialmediaapp/settings.py
+++ socialmediaapp/settings.py
@@ -148,6 +148,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'   ,# or whatever port your React app is running on
+# or whatever port your React app is running on
     'https://thesocialcomapny.vercel.app'
 ]
