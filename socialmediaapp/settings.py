a33fc95c80e5bbe7a1473daa33a85d1850289349
Vishva15Vishva2003 <vishvavishva587@gmail.com>
2024-09-12 12:09:15 +0530

    add paths

--- socialmediaapp/settings.py
+++ socialmediaapp/settings.py
@@ -138,7 +138,9 @@ ALLOWED_HOSTS = ['*']
 STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- socialmediaapp/urls.py
+++ socialmediaapp/urls.py
@@ -16,12 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
+def home(request):
+    return HttpResponse("Welcome to the Social Company Backend!")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('products.urls'))
+    path('api/', include('products.urls')),
+    path('', include('products.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
